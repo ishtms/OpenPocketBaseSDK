ba3ab8d321f948d36d1364901bbe2bcd4c7ad4d4
Ishtmeet Singh <hello@ishtms.com>
2026-07-26 00:00:50 +0530

    adds stalled fixture response

--- Tests/Integration/pb_hooks/openpocketbase_test_routes.pb.js
+++ Tests/Integration/pb_hooks/openpocketbase_test_routes.pb.js
@@ -5,3 +5,13 @@ routerAdd("GET", "/api/openpocketbase-test/delay", (event) => {
     delayMilliseconds: 2500,
   });
 });
+
+routerAdd("GET", "/api/openpocketbase-test/stall", (event) => {
+  event.response.header().set("Content-Type", "application/json");
+  event.response.writeHeader(200);
+  event.response.write(toBytes('{"phase":"started",'));
+  event.flush();
+  sleep(2500);
+  event.response.write(toBytes('"done":true}'));
+  event.flush();
+});
